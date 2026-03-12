207534e155a64581c3ae4e8a9a7de4e7e9b3b033
Jeeva8525 <jeevananthan8525@gmail.com>
2026-03-12 11:12:07 +0530

    normal user component

--- lab7/library-react/src/components/Home/Home.jsx
+++ lab7/library-react/src/components/Home/Home.jsx
@@ -1,23 +1,75 @@
+import { useState } from 'react'
 import './Home.css'
 export default function Home({books, setBooks}){
 
+    const [searchItem, setSearchItem] = useState('');
+
+    function handleIssueBook(bid){
+        setBooks(
+            books.map((b) => {
+                if(b.id === bid && b.isIssued===false)
+                {
+                    return {...b ,isIssued : true}
+                }
+                return b
+            })
+        )
+    }
+
+    let filteredBooks;
+    if( searchItem !== ""){
+        filteredBooks = books.filter((b) => {
+            if(b.name.toLowerCase().includes(searchItem.toLowerCase())){
+                return true;
+            }
+        })
+    }
+    else{
+        filteredBooks=books;
+    }
+
+    function handleReturnBook(bid){
+        setBooks(
+            books.map((b) => {
+                if(b.id === bid && b.isIssued===true)
+                {
+                    return {...b ,isIssued : false}
+                }
+                return b
+            })
+        )
+    }
+
+    function handleSearchFilter(name){
+        setSearchItem(name);
+    }
+
     return (
         <>
+            <div className="search-container">
+                <input 
+                    type="text" 
+                    placeholder='Search Book'
+                    onInput={(event)=>handleSearchFilter(event.target.value)}
+                />
+            </div>
+
             {
-                books.map((b) => {
+                filteredBooks.map((b) => {
                     return (
-                        <div className="book">
-                            <p>Title : {b.name}</p>
+                        <div className="book" key={b.id}>
+                            <p>Title : {b.name}</p> 
+
                             {b.isIssued &&
                                 <>
                                     <p> Status : Issued</p>
-                                    <button>Return book</button>
+                                    <button onClick={()=>handleReturnBook(b.id)}>Return book</button>
                                 </>
                              }
                             {!b.isIssued && 
                                 <>
                                     <p> Status : Available</p>
-                                    <button>Issue Book</button> 
+                                    <button onClick={()=>handleIssueBook(b.id)}>Issue Book</button> 
                                 </>
                             }
 
